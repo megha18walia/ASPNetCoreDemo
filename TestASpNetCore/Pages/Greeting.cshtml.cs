6b20f3240bee34292be289a442680c951a862435
Megha Walia <mwalia2@sapient.com>
2018-09-03 12:04:20 +0000

    code committed for razor view

---
+++ TestASpNetCore/Pages/Greeting.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestASpNetCore.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        public string Message { get; set; }
+        private IGreeting _greet;
+
+
+        public GreetingModel(IGreeting greeting)
+        {
+            _greet = greeting;
+        }
+
+        public void OnGet(string name)
+        {
+            Message =$"{name} : {_greet.GetMessage()}";
+        }
+    }
+}
